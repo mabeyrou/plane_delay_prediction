82b0ec81ccbe413e2881d20e5da13319e561e703
mroussel <marion.roussel@observia-group.com>
2025-07-27 15:53:49 +0200

    ci/cd: add a .env.test file for test database

--- data_api/tests/__init__.py
+++ data_api/tests/__init__.py
@@ -1,10 +1,9 @@
 import os
 import sys
+from dotenv import load_dotenv
 
-sys.path.insert(0,
-                os.path.abspath(
-                    os.path.join(
-                        os.path.dirname(__file__),
-                        ".."
-                    )
-                ))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+# def pytest_configure():
+#     load_dotenv(dotenv_path=".env.test")
